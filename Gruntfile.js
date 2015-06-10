90ad34f0dfc103419ae0988d66564402779954e3
Joel Holder <jclosure@gmail.com>
2015-06-09 22:37:50 -0500

    added grunt-mocha-test for main runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,13 @@ module.exports = function(grunt){
   banner += '- <%= pkg.description %>n<%= pkg.repository.url %>n';
   banner += 'Built on <%= grunt.template.today("yyyy-mm-dd") %>n*/n';
 
+  // Load grunt mocha task
+  grunt.loadNpmTasks('grunt-mocha');
+
+  
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+
   
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -15,18 +22,29 @@ module.exports = function(grunt){
         src: ['test/testrunner.html'],
       },
       options: {
-        run: true
-        //,
-        // mocha: {
-        //   "debug-brk": (grunt.option('debug-brk')) ? "" : 0
-        // }
+        run: true,
+        require: [
+          'models/entity', //from proj root
+          //'./globals.js',
+          //function(){ testVar1=require('./stuff'); },
+          //function(){ testVar2='other-stuff'; }
+        ]
+      }
+    },
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt',
+          quiet: false
+        },
+        src: ['test/**/*.js']
       }
     }
   });
 
-  // Load grunt mocha task
-  grunt.loadNpmTasks('grunt-mocha');
 
-  grunt.registerTask('default', ['mocha']);
+  grunt.registerTask('default', ['mocha','mochaTest']);
 };
 
---
+++ bin/test.bat
@@ -0,0 +1,8 @@
+@echo off
+
+
+set NEO4J_URL=http://neo4j:admin@localhost:7474
+
+
+mocha
+
